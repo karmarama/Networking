8c07152a973fc3975f4f0fbe9e2545ca500ac733
Sam Woolf <sam.woolf@nice.agency>
2019-07-15 14:59:36 +0100

    add targets for watch and tvOS

---
+++ Networking-watch/Networking_watch.h
@@ -0,0 +1,13 @@
+//// Created by Nice Agency
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Networking_watch.
+FOUNDATION_EXPORT double Networking_watchVersionNumber;
+
+//! Project version string for Networking_watch.
+FOUNDATION_EXPORT const unsigned char Networking_watchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking_watch/PublicHeader.h>
+
+
