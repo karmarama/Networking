2db2b82c939ccd83f4466c1d2496be8ad40e2252
Sam Woolf <sam.woolf@nice.agency>
2019-07-15 14:51:05 +0100

    add mac cocoa touch target

---
+++ NetworkingMac/NetworkingMac.h
@@ -0,0 +1,13 @@
+//// Created by Nice Agency
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NetworkingMac.
+FOUNDATION_EXPORT double NetworkingMacVersionNumber;
+
+//! Project version string for NetworkingMac.
+FOUNDATION_EXPORT const unsigned char NetworkingMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingMac/PublicHeader.h>
+
+
