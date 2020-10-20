cbf07d16ac7a702c463f67f7cacc70dc010338c9
Abel <abel.castro@karmarama.com>
2020-10-20 15:17:33 +0100

    Added operationQueue to future(for resource:) signature Added queue to load and .receive(on: )

--- Sources/Webservice/ResourceRequestable+Combine.swift
+++ Sources/Webservice/ResourceRequestable+Combine.swift
@@ -4,9 +4,11 @@ import Foundation
 @available(iOS 13.0, *)
 extension ResourceRequestable {
 
-    func future<Request, Response>(for resource: Resource<Request, Response>) -> AnyPublisher<Response, Swift.Error> {
-        return Future { completion in
-            load(resource, completion: completion)
-        }.eraseToAnyPublisher()
+    func future<Request, Response>(for resource: Resource<Request, Response>,
+                                   queue: OperationQueue = .main) -> AnyPublisher<Response, Swift.Error> {
+        Future { completion in
+            load(resource, queue: queue, completion: completion)
+        }.receive(on: queue)
+        .eraseToAnyPublisher()
     }
 }
